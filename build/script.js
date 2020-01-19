c36e156bc7bf1e3022ff48d7003829cf84658090
Alexander Mamutov <alexander.mamutov@gmail.com>
2020-01-19 14:30:26 +0400

    Fix script error `addEventListener of undefined`

--- build/script.js
+++ build/script.js
@@ -1,20 +1,30 @@
-document.querySelector('.onoffswitch').addEventListener('click', () => {
-	document.querySelector('.onoffswitch').classList.toggle('onoffswitch_checked');
+const onoffswitchElements = document.querySelectorAll('.onoffswitch');
+for (const onoffswitchElement of onoffswitchElements) {
+	onoffswitchElement.addEventListener('click', () => {
+		onoffswitchElement.classList.toggle('onoffswitch_checked');
 
-
-	const theme = document.querySelector('.theme');
-	if (theme.classList.contains('theme_color_project-default')) {
-		theme.classList.remove('theme_color_project-default');
-		theme.classList.add('theme_color_project-inverse');
-	} else {
-		theme.classList.add('theme_color_project-default');
-		theme.classList.remove('theme_color_project-inverse');
-	}
-});
-
-const accordion_items = document.querySelectorAll('.e-accordion');
-Array.from(accordion_items).forEach(accordion_item => {
-	accordion_item.querySelector('.e-accordion__short').addEventListener('click', () => {
-		accordion_item.querySelector('.e-accordion__more').classList.toggle('history__hide');
+		const theme = document.querySelector('.theme');
+		if (theme) {
+			if (theme.classList.contains('theme_color_project-default')) {
+				theme.classList.remove('theme_color_project-default');
+				theme.classList.add('theme_color_project-inverse');
+			} else {
+				theme.classList.add('theme_color_project-default');
+				theme.classList.remove('theme_color_project-inverse');
+			}
+		}
 	});
-});
+}
+
+const accordionItems = document.querySelectorAll('.e-accordion');
+for (const accordionItem of accordionItems) {
+	const short = accordionItem.querySelector('.e-accordion__short');
+	if (short) {
+		short.addEventListener('click', () => {
+			const more = accordionItem.querySelector('.e-accordion__more');
+			if (more) {
+				more.classList.toggle('history__hide');
+			}
+		});
+	}
+};
